6df8b93ac08b84f215d6ea5447e1ea3c63eabfc1
Shiva Shrestha <64135683+sh7vashrestha@users.noreply.github.com>
2020-09-30 17:23:44 +0545

    change

--- Website/calc/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Todo(models.Model):
-    work = models.CharField(max_length=100)
-    time = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.work
--- Website/website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from calc import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('calc.urls')),
-]
